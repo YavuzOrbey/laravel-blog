c52046fdf5f7f4d827ea619ff8132727a17c84ef
Yavuz Orbey <yavuz.orbey@gmail.com>
2018-12-06 23:49:26 +0300

    Edit Form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,17 +3,20 @@
 @section('content')
     <div class="row justify-content-center">
         <div class="col-md-8"><h1>Create New Post</h1></div>
-        <form action="">
-            <div class="form-group">
-                <label for="theTitle">Title</label>
-                <input class="form-control" type="text" id="theTitle" placeholder="Enter a Title">
-            </div>
-            <div class="form-group">
-                <label for="theBody">Body</label>
-                <textarea class="form-control" placeholder="What do you want to talk about?"></textarea>
-            </div>
-            <input type="submit" value="Submit" class="btn btn-primary" >
-        </form>
+        <div class="col-md-12">
+            <form action="">
+                <div class="form-group">
+                    <label for="theTitle">Title</label>
+                    <input class="form-control" type="text" id="theTitle" placeholder="Enter a Title">
+                </div>
+                <div class="form-group">
+                    <label for="theBody">Body</label>
+                    <textarea class="form-control" placeholder="What do you want to talk about?"></textarea>
+                </div>
+                <input type="submit" value="Submit" class="btn btn-primary" >
+            </form>
+        </div>
+        
     </div>
 @endsection
 @section('title', '| Create Post')
