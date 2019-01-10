c187e7008e144918663f536b5a05dab97f5db064
Yavuz <yavuz.orbey@gmail.com>
2019-01-10 15:36:08 -0500

    Testing ssh

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('about', 'PagesController@getAbout');
 Route::get('contact', 'ContactController@create');
 Route::post('contact', 'ContactController@store')->name('contact.store'); //route name for controller action
 Route::get('blog/{slug}', 'BlogController@getSingle')->middleware('web')->name('blog.single');
-Route::resource('posts','PostController');
+Route::resource('posts','PostController'); // testing purposes!!
