af757837693a8026157df0440aba69cd0a56c70d
Yavuz <yavuz.orbey@gmail.com>
2019-01-21 20:47:01 -0500

    commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,6 +62,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
+    // dasds
     protected function create(array $data)
     {
         return User::create([
