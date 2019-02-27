9ea05e17830a25b205cf36f770047ee2f5bbcf4b
Yavuz <yavuz.orbey@gmail.com>
2019-02-27 14:21:19 -0500

    add gravatar icons

--- resources/views/inc/_load_comments.blade.php
+++ resources/views/inc/_load_comments.blade.php
@@ -5,10 +5,10 @@
         @foreach($post->comments as $comment) 
         <li class="comment">
             <div class="row">
-                <div class="col-xs-2">
-                <div class="portrait-icon">{{"ICON"}}</div>
+                <div class="col-sm-auto portrait-wrapper">
+                <img class="portrait-icon" src="{{ 'https://www.gravatar.com/avatar/' .  md5( strtolower( trim($comment->user->email)))}}">
                 </div>
-                <div class="col-xs-10 comment-banner">
+                <div class="col-sm-10 comment-banner">
                     <div class="row">
                         <div class="col-sm-6 username">{{$comment->user->username   /*$users[$key]->username didn't even need to send the users */}}</div>
                         <div class="col-sm-6 date-time-display">{{ date('m/d/Y g:i A', strtotime($comment->created_at)) . ($comment->created_at == $comment->updated_at ? ' ': ' Edited on: ' . date('m/d/Y, g:i A', strtotime($comment->updated_at)))}}</div>
@@ -16,17 +16,19 @@
                 </div>
             </div>
             <div class="row mt-3">
-                <div class="col-xs-12">
+            <div class="col-sm-{{ Auth::user() == $comment->user ? '10': '12'}}">
                     {{$comment->comment_text}}
                 </div>
+                @if(Auth::user() == $comment->user)
+                <div class="col-sm-2">
+                    {!! Form::open(['route'=> ['comments.destroy', $comment], 'method'=>'DELETE']) !!}
+                    {{Form::submit('Delete', array('class'=>'btn btn-danger btn-block')) }}
+                    {!! Form::close() !!}
+                </div>
+                
+                @endif
             </div>
-            @if(Auth::user() == $comment->user)
-            {!! Form::open(['route'=> ['comments.destroy', $comment], 'method'=>'DELETE']) !!}
-            <div class="row">
-                <div class="col-sm-4">{{Form::submit('Delete', array('class'=>'btn btn-danger btn-block')) }}</div>
-            </div>
-            {!! Form::close() !!}
-            @endif
+            
         </li>
         @endforeach
         </ul>
